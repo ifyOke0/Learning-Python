8045684502d2d2e9dbe7881dbfd7ee241508ace2
Henry Okeke <henry@users.noreply.github.com>
2025-04-15 23:51:43 +0100

    Merge remote-tracking branch 'origin/main'

--- simple_py_program/Herztext.py
+++ simple_py_program/Herztext.py
@@ -1,11 +1,8 @@
-output = '\n'.join([
+print('\n'.join([
     ''.join([
         (' Pride  '[(x - y) % 8] if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0 else ' ')
         for x in range(-30, 30)
     ])
     for y in range(15, -15, -1)
-])
+]))
 
-# Save output to the root folder
-with open("output_Herztext.md", "w", encoding="utf-8") as f:
-    f.write("```\n" + output + "```\n")
--- simple_py_program/Sanduhr.py
+++ simple_py_program/Sanduhr.py
@@ -5,14 +5,5 @@ def Sanduhr_muster(n):
     for i in range(2, n, 1):
         print(" " * (n - i) + "* " * i)
 
-output = ""  
 
-# Capture output into the variable
-for i in range(5, 0, -1):
-    output += " " * (5 - i) + "* " * i + "\n"
-for i in range(2, 5):
-    output += " " * (5 - i) + "* " * i + "\n"
-
-# Save output to the root folder
-with open("output_Sanduhr.md", "w", encoding="utf-8") as f:
-    f.write("```\n" + output + "```\n")
+Sanduhr_muster(5)
