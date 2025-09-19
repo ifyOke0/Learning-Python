c6e4c52dffc73523f5e40972d41b9a24b58b91ce
TheM1ddleMan <192749026+ModuleMaster64@users.noreply.github.com>
2025-09-19 21:56:50 +0100

    Update passwort.py
    updated and kept it german

--- simple_py_program/passwort.py
+++ simple_py_program/passwort.py
@@ -1,9 +1,10 @@
-#####Practice 4
-##Simple Password Checker
-Passwort_prüfen = input("Passwort eingeben: ")
-hat_Nummer = any(char.isdigit() for char in Passwort_prufen)
+##### Practice 4
+## Simple Password Checker
 
-if len(Passwort_prüfen) > 8 and hat_Nummer:
+password = input("Passwort eingeben: ")
+has_number = any(char.isdigit() for char in password)
+
+if len(password) > 8 and has_number:
     print("starkes Passwort")
 else:
-    print("schwaches Passwort!, Versuch es noch einmal...")
+    print("schwaches Passwort! Versuch es noch einmal...")
