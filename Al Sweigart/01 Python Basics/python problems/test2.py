daa4fd5f6e8579da52c1c5d03dbccad244c9c341
Ifeanyi <okeke.henry.37046473@gmail.com>
2025-09-24 12:04:46 +0100

    finished python basics

---
+++ Al Sweigart/01 Python Basics/python problems/test2.py
@@ -0,0 +1,3 @@
+# Modify test1: also ask for their birth month & day, and tell them whether they've had their birthday
+# yet this year (relative to today). (Work with date arithmetic manually or use datetime.)
+
